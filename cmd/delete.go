f88c2efd5acf9669e8ebef7dfb492bbc05932a22
njten <nemec.jan42@seznam.cz>
2026-04-07 20:32:43 +0200

    problem changed

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -16,20 +15,21 @@ var deleteCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		force, _ := cmd.Flags().GetBool("force")
-		
+
 		if !force {
-			fmt.Println("Are you sure you want to permanently delete vault '%s'? [y/N]: ", args[0])
-			reader := bufio.NewReader(os.Stdin)
-			input, _ := reader.ReadString('\n')
+			fmt.Printf("Are you sure you want to permanently delete vault '%s'? [y/N]: ", args[0])
+			var input string
+			fmt.Scan(&input)
 			input = strings.TrimSpace(strings.ToLower(input))
-			if input != "y\n" {
-				fmt.Println("Aborting")
+			if input != "y" {
+				fmt.Println("Aborted.")
 				return
 			}
 		}
+
 		if err := vault.Delete(args[0]); err != nil {
-			fmt.Fprintln(os.Stderr, "error", err)
-				os.Exit(1)
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
 		}
 	},
 }
